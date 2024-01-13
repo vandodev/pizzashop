64e69cfb364c10997a39634221329166a3cdd2ed
vandodev <vandoaparecido@hotmail.com>
2024-01-13 08:12:17 -0300

    :pizza: :hamburger: :fork_and_knife: Configurando o shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from './components/ui/button'
+import './global.css'
 
 export function App() {
 
   return( 
-    <h1>Olá mundo</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
+    </div>
   );
 }
 
